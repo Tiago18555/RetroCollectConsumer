53e9be649887d60b9e94691cade5e85b96cd112f
Tiago18555 <tcajueiro_1995@hotmail.com>
2024-11-09 04:04:34 -0300

    2.0.4: test/fix refacttor I

--- Domain/Entities/ComputerCollectionItem.cs
+++ Domain/Entities/ComputerCollectionItem.cs
@@ -1,19 +1,32 @@
 ﻿using Domain.Enums;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Domain.Entities;
 
+[Table("computer_collections")]
 public class ComputerCollectionItem
 {
+    [Column("id")]
     [Key]
     public Guid Id { get; set; }
 
+    [Column("condition")]
     public Condition Condition { get; set; }
+
+    [Column("purchase_date")]
     public DateTime PurchaseDate { get; set; }
+
+    [Column("notes")]
     public string Notes { get; set; }
+
+    [Column("ownership_status")]
     public OwnershipStatus OwnershipStatus { get; set; }
 
+    [Column("user_id")]
     public Guid UserId { get; set; }
+
+    [Column("computer_id")]
     public int ComputerId { get; set; }
     public User User { get; set; }
     public Computer Computer { get; set; }
--- Domain/Entities/User.cs
+++ Domain/Entities/User.cs
@@ -1,31 +1,46 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Domain.Entities;
 
+[Table("users")]
 public class User
 {
+    [Column("user_id")]
     [Key]
     public Guid UserId { get; set; }
 
+    [Column("username")]
     [Required]
     [MinLength(3)]
-    [MaxLength(255)]
+    [MaxLength(64)]
     public string Username { get; set; }
+
+    [Column("password")]
     public string Password { get; set; }
 
+    [Column("email")]
     [MinLength(3)]
     [MaxLength(255)]
     public string Email { get; set; }
+
+    [Column("verified_at")]
     public DateTime VerifiedAt { get; set; }
 
+    [Column("first_name")]
     [MinLength(3)]
     [MaxLength(255)]
     public string FirstName { get; set; }
 
+    [Column("last_name")]
     [MinLength(3)]
     [MaxLength(255)]
     public string LastName { get; set; }
+
+    [Column("created_at")]
     public DateTime CreatedAt { get; set; }
+
+    [Column("updated_at")]
     public DateTime UpdatedAt { get; set; }
     public IEnumerable<ConsoleCollectionItem> UserConsoles { get; set; }
     public IEnumerable<ComputerCollectionItem> UserComputers { get; set; }
